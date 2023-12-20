b8939855eb257ad9089fb525b52fcdd84074d9f8
matchbean111 <1224231492@qq.com>
2023-12-20 21:10:40 +0800

    我只是机械的抄写了这份代码，我忏悔

--- main21.c
+++ main21.c
@@ -14,3 +14,117 @@ typedef struct Graph {
     int vexNum;
     int arcNum;
 } Graph;
+
+typedef struct Edge {
+    int start;
+    int end;
+    int weight;
+} Edge;
+
+Edge *initEdge(Graph *G) {
+    Edge *edge = (Edge *) malloc(sizeof(Edge) * G->arcNum);
+    int index = 0;
+    for (int i = 0; i < G->vexNum; i++) {
+        for (int j = i + 1; j < G->vexNum; j++) {
+            if (G->arcs[i][j] != MAX) {
+                edge[index].start = i;
+                edge[index].end = j;
+                edge[index].weight = G->arcs[i][j];
+                index++;
+            }
+        }
+    }
+    return edge;
+}
+
+void sortEdge(Edge *edge, Graph *G) {
+    Edge temp;
+    for (int i = 0; i < G->arcNum - 1; i++) {
+        for (int j = 0; j < G->arcNum - 1 - i; j++) {
+            if (edge[j].weight > edge[j + 1].weight) {
+                temp = edge[j];
+                edge[j] = edge[j + 1];
+                edge[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void kruskal(Graph *G) {
+    int *connected = (int *) malloc(sizeof(int) * G->vexNum);
+    for (int i = 0; i < G->vexNum; i++) {
+        connected[i] = i;
+    }
+    Edge *edge = initEdge(G);
+    sortEdge(edge, G);
+
+    for (int i = 0; i < G->arcNum; i++) {
+        int start = connected[edge[i].start];
+        int end = connected[edge[i].end];
+        if (start != end) {
+            printf("v%c --> v%c weight = %d\n",G->vexs[edge[i].start],
+                   G->vexs[edge[i].end],edge[i].weight);
+            for (int j = 0; j < G->vexNum; j++) {
+                if (connected[j] == end){
+                    connected[j] = start;
+                }
+            }
+        }
+    }
+}
+
+Graph *initGraph(int vexNum) {
+    Graph *G = (Graph *) malloc(sizeof(Graph));
+    G->vexs = (char *) malloc(sizeof(char) * vexNum);
+    G->arcs = (int **) malloc(sizeof(int *) * vexNum);
+    for (int i = 0; i < vexNum; i++) {
+        G->arcs[i] = (int *) malloc(sizeof(int) * vexNum);
+    }
+
+    G->vexNum = vexNum;
+    G->arcNum = 0;
+    return G;
+}
+
+void createGraph(Graph *G, char *vexs, int *arcs) {
+    for (int i = 0; i < G->vexNum; i++) {
+        G->vexs[i] = vexs[i];
+        for (int j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = *(arcs + i * G->vexNum + j);
+            if (G->arcs[i][j] != 0 && G->arcs[i][j] != MAX) {
+                G->arcNum++;
+            }
+        }
+    }
+    G->arcNum /= 2;
+}
+
+void DFS(Graph *G, int *visited, int vexIndex) {
+    printf("%c\t", G->vexs[vexIndex]);
+    visited[vexIndex] = 1;
+    for (int i = 0; i < G->vexNum; i++) {
+        if (G->arcs[vexIndex][i] > 0 && G->arcs[vexIndex][i] != MAX && !visited[i]) {
+            DFS(G, visited, i);
+        }
+    }
+}
+
+int main() {
+    Graph *G = initGraph(6);
+    int *visited = malloc(sizeof(int) * G->vexNum);
+    for (int i = 0; i < G->vexNum; i++) {
+        visited[i] = 0;
+    }
+    int arcs[6][6] = {
+            0, 6, 1, 5, MAX, MAX,
+            6, 0, 5, MAX, 3, MAX,
+            1, 5, 0, 5, 6, 4,
+            5, MAX, 5, 0, MAX, 2,
+            MAX, 3, 6, MAX, 0, 6,
+            MAX, MAX, 4, 2, 6, 0
+    };
+    createGraph(G, "123456", (int *) arcs);
+    DFS(G, visited, 0);
+    printf("\n");
+    kruskal(G);
+}
