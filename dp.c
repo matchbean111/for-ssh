73c7da932f72742afe3605a1b9056c9769d814fd
matchbean111 <1224231492@qq.com>
2024-01-10 04:10:56 +0800

    Attention!!!!!

--- dp.c
+++ dp.c
@@ -3,8 +3,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int f(int n);
 
-    return 0;
+int g(int n);
+
+
+int main(int argc, char *argv[]) {
+//    int n = strtol(argv[1],);
+//    int a = f(n);
+//
+//    printf("%d\n", a);
+}
+
+int g(int n) {
+    if (n <= 1) return 1;
+    return f(n + 1) + g(n - 1);
+}
+
+int f(int n) {
+    if (n <= 1) return 1;
+    return f(n - 1) + g(n - 2);
 }
